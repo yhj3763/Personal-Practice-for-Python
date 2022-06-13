4f01f9dfdcd1dcf0bf39f1526b53f6202fd0b0a6
YHJ <yaj3763@naver.com>
2022-06-12 23:38:14 -0700

    Creating Frame

---
+++ python_gui_project/1._create_frame.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk()
+root.title("GUI")
+root.geometry("640x480") # width and height
+
+root.resizable(False, False) # width and height are unchangable
+root.mainloop()
