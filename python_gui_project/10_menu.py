b67328e6b84d186f167f889312c1ca5253382077
YHJ <yaj3763@naver.com>
2022-06-21 23:26:17 -0700

    Menu

--- python_gui_project/10_menu.py
+++ python_gui_project/10_menu.py
@@ -6,14 +6,38 @@ root = Tk()
 root.title("GUI")
 root.geometry("640x480") # width and height
 
+def create_new_file():
+    print("Create New File")
+
+menu = Menu(root)
+
+#File menu
+menu_file = Menu(menu, tearoff = 0)
+menu_file.add_command(label="New File", command=create_new_file)
+menu_file.add_command(label="New Window")
+menu_file.add_separator()
+menu_file.add_command(label="Open File...")
+menu_file.add_separator()
+menu_file.add_command(label="Save All", state="disable")
+menu_file.add_separator()
+menu_file.add_command(label="Exit", command=root.quit)
+menu.add_cascade(label="File", menu=menu_file)
+
+# Edit menu(Empty value)
+menu.add_cascade(label="Edit")
+
+# Language menu
+menu_lang = Menu(menu, tearoff=0)
+menu_lang.add_radiobutton(label="Python")
+menu_lang.add_radiobutton(label="Java")
+menu_lang.add_radiobutton(label="C++")
+menu.add_cascade(label="Language", menu=menu_lang)
+
+# View menu
+menu_view = Menu(menu, tearoff=0)
+menu_view.add_checkbutton(label="Show Minimap")
+menu.add_cascade(label="View", menu=menu_view )
 
 
-def btncmd():
-    pass
-
-btn = Button(root, text="Stop", comman=btncmd)
-btn.pack()
-
-
-
+root.config(menu = menu)
 root.mainloop()
---
+++ python_gui_project/11_messagebox.py
@@ -0,0 +1,10 @@
+import time
+import tkinter.ttk as ttk
+from tkinter import *
+
+root = Tk()
+root.title("GUI")
+root.geometry("640x480") # width and height
+
+
+root.mainloop()
